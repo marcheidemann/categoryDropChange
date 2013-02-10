3c33280d864602852be7045dcc5ff8fc5e2662fb
Marc Heidemann <marc.heidemann@opus.edu.library.microle-systems.com>
2013-02-11 00:42:11 +0100

    PHP: Console log edit

--- ajax.php
+++ ajax.php
@@ -3,6 +3,8 @@ function doIt($callback) { $callback(); }
 	doIt(function() {
 	     $stringData = $_POST['className']; 
 	     $stringData = strtoupper($stringData);
+	     echo 'Hello from PHP Serverside: ';
+	     echo ' Your Selected ChangeClass, Uppercased inside PHP  was: ';
 	     echo $stringData;
 	});
 ?>
